8e3cbf4203788772539e26996f2c61daf533f473
ziasana <zs.hossainy@gmail.com>
2025-11-28 13:12:21 +0100

    Added flapdoodle plugin

--- src/test/java/org/example/dictionaryapi/DictionaryApiApplicationTests.java
+++ src/test/java/org/example/dictionaryapi/DictionaryApiApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.dictionaryapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@ActiveProfiles("test")
 class DictionaryApiApplicationTests {
 
     @Test
